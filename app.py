aaf593214db7d1d8d6c7a5d7e1bd8836b8b06dff
pratikdesai399 <pratikdesai399@gmail.com>
2022-02-20 15:04:27 +0530

    Server Functioning

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from distutils.log import debug
+from unittest.util import _MAX_LENGTH
 from flask import Flask, jsonify
+import sys
+from transformers import pipeline
 
 app = Flask(__name__)
 
@@ -9,6 +12,21 @@ def hello():
         "Hello":"WhatsNxt!"
     })
     return res
+
+@app.route("/autocomplete")
+def autocomplete():
+    modelPipeline = pipeline('text-generation', model = 'D:\COEP\Final Year\BTech Project\model\output')
+
+    #sample context
+    context = "@rhugaved: How are"
+    result = modelPipeline(context, max_length=20, num_return_sequences=5, do_sample=True, eos_token_id=2, pad_token_id=0, skip_special_tokens=True, top_k=50, top_p=0.95)
+    print("Result: {}".format(result))
+
+    res = jsonify({
+        "AUTOCOMPLETE: ":result
+    })
+    return res
+
 
 if __name__ == "__main__":
     app.run(debug=True)
