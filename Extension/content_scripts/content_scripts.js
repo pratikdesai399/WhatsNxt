953c809c080d4810ce7399b8bf811c997eb42137
pratikdesai399 <pratikdesai399@gmail.com>
2022-02-20 20:20:38 +0530

    Tab Key Pressed working for one chat as of now

--- Extension/content_scripts/content_scripts.js
+++ Extension/content_scripts/content_scripts.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
     console.log("WhatsNxt?");
-
     tabKeyPress = false;
 
     var interval = setInterval(function(){
+        console.log("Loading...")
         if(tabKeyPress == false){
-            //User has typed some words
-            console.log(tabKeyPress);
             if($('[data-tab="10"]').length > 0){
-                console.log($('[data-tab="10"]'));
-                console.log("Event Listerner");
+                //console.log($('[data-tab="10"]'));
+                //console.log("Event Listerner");
                 $('[data-tab="10"]').on('keydown', function(e){
                     if(e.keyCode == 9){
                         e.stopPropagation();
                         e.preventDefault();
+                        //tabKeyPress = true;
+                        //console.log(tabKeyPress);
+                        $('[data-tab="10"]').blur();
                         console.log("TAB KEY PRESSED");
-                        
+                        //Generate Prompts
 
                     }
                 });
+
                 tabKeyPress = true;
-                console.log("True Down");
-                clearInterval(interval);
+                console.log("Aha Tab key pressed is true now")
+                //clearInterval(interval);
             }
         }
     }, 1000);
